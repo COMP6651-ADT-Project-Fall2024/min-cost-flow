ee61109a38741d9d4d060e815f0684205f1ac464
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-12-07 15:20:35 -0500

    edmondskarp maxf implementation

--- src/main/java/MinCostFlow.java
+++ src/main/java/MinCostFlow.java
@@ -1,13 +1,17 @@
 import graph.GenerateGraph;
+import graph.Graph;
+import simulations.EdmondsKarp;
+import util.GraphUtils;
 
 import java.io.IOException;
 import java.util.Scanner;
+
 public class MinCostFlow {
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome!!");
-        System.out.println("For Graph generation press 1 for testing algo press 2");
+        System.out.println("For Graph generation press 1 for testing algo press 2 and for simulation 3");
         String user_input = scanner.nextLine();
         if (user_input.equals("1")){
             GenerateGraph.generateSinkSourceGraph(10, 0.8, 5, 4);
@@ -18,6 +22,16 @@ public class MinCostFlow {
             String algoChoice = scanner.nextLine();
             if(algoChoice.equals("1")){
                 driver.primalDualDriver("graph3.txt");
+            }
+        }
+        else if(user_input.equals("3")){
+            System.out.println("Choose which Simulation to run: \n1. Simulation1");
+            String algoChoice = scanner.nextLine();
+            if(algoChoice.equals("1")){
+                EdmondsKarp edmondsKarpObj = new EdmondsKarp();
+                Graph graph = GraphUtils.loadGraph("graph4.txt", 1, 7);
+                int maxFlow = edmondsKarpObj.maxFlow(graph, 1, 7);
+                System.out.println(maxFlow);
             }
         }
 
